0f1710c6008c7f813c3d54c45049185dcfc9dfa8
kristina-kryg <kristina.krygowska@gmail.com>
2026-08-22 15:04:52 +0200

    feat: input field added

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const exchangeRate = prompt("Enter the exchange rate");
-document.write("The current exchange rate is " + exchangeRate + "<br>");
+// const feePercentage = 0.29;
+// function calculateFee(exchangeRate, feePercentage) {
+//     const fee = (exchangeRate * feePercentage) / 100;
+//     return fee;
+// }
+// const fee = calculateFee(exchangeRate,feePercentage);
+// document.write("The commission fee amount is " + fee.toFixed(4) + "<br>");
 
-const feePercentage = 0.29;
-function calculateFee(exchangeRate, feePercentage) {
-    const fee = (exchangeRate * feePercentage) / 100;
-    return fee;
+// const maxExchangeRate = exchangeRate - fee;
+// document.write("Maximum social exchange rate is " + maxExchangeRate.toFixed(4) + "<br>");
+
+function calculateFeeClicked() {
+    const exchangeRateInput = document.getElementById("exchange-rate");
+    const exchangeRate = exchangeRateInput.value;
+    console.log("User entered exchange rate: " + exchangeRate);
 }
-const fee = calculateFee(exchangeRate,feePercentage);
-document.write("The commission fee amount is " + fee.toFixed(4) + "<br>");
-
-const maxExchangeRate = exchangeRate - fee;
-document.write("Maximum social exchange rate is " + maxExchangeRate.toFixed(4) + "<br>");
