ffd234ac1aabd366a62b68372daafe3b1f50e965
kristina-kryg <kristina.krygowska@gmail.com>
2026-09-07 18:22:16 +0200

    feat:added social exchange

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 function calculateFee(exchangeRate, feePercentage) {
-    const fee = (exchangeRate * feePercentage) / 100;
-    return fee;
+  const fee = (exchangeRate * feePercentage) / 100;
+  return fee;
 }
 
-function calculateFeeFromInputs() {
-    const exchangeRateValue = document.getElementById("exchange-rate").value;
-    const feePercentageValue = document.getElementById("fee").value;
-    
-    const fee = calculateFee(exchangeRateValue, feePercentageValue);
-
-   const result = document.getElementById("fee-result");
-    result.innerHTML = "The commission fee amount is: <br><strong>" + fee.toFixed(4) + "</strong>";
+function calculateTotalSocialRate() {
+  const exchangeRateValue = document.getElementById("social-rate").value;
+  const feePercentageValue = document.getElementById("fee").value;
+  return (
+    calculateFee(exchangeRateValue, feePercentageValue) +
+    Number(exchangeRateValue)
+  );
 }
+
+const showResult = () => {
+  const result = document.getElementById("result-container");
+  const totalSocialRate = calculateTotalSocialRate();
+  result.innerHTML =
+    "The total social rate is: <br><strong>" +
+    totalSocialRate.toFixed(3) +
+    "</strong>";
+};
+
 function clearInputs() {
-    document.getElementById("fee").value = null;
-    document.getElementById("exchange-rate").value = null;
-    document.getElementById("fee-result").innerHTML = "";
+  document.getElementById("fee").value = null;
+  document.getElementById("social-rate").value = null;
+  document.getElementById("standard-rate").value = null;
+  document.getElementById("fee-result").innerHTML = "";
 }
