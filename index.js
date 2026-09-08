85ab3e8e0278de74e6541ce926dd73cdcb7cfab5
kristina-kryg <kristina.krygowska@gmail.com>
2026-09-08 13:58:50 +0200

    feat: comparing rates

--- index.js
+++ index.js
@@ -8,22 +8,34 @@ function calculateTotalSocialRate() {
   const feePercentageValue = document.getElementById("fee").value;
   return (
     calculateFee(exchangeRateValue, feePercentageValue) +
-    Number(exchangeRateValue)
+    parseFloat(exchangeRateValue)
   );
 }
 
 const showResult = () => {
   const result = document.getElementById("result-container");
   const totalSocialRate = calculateTotalSocialRate();
-  result.innerHTML =
-    "The total social rate is: <br><strong>" +
-    totalSocialRate.toFixed(3) +
-    "</strong>";
+  result.innerHTML = `The total social rate is: <br><strong> 
+    ${totalSocialRate.toFixed(3)}</strong><br>`;
+  const standardRate = parseFloat(
+    document.getElementById("standard-rate").value,
+  );
+  result.innerHTML += compareRates(totalSocialRate, standardRate);
 };
+
+function compareRates(totalSocialRate, standardRate) {
+  if (totalSocialRate < standardRate) {
+    return `The most profitable rate for exchange is <strong>social rate</strong>.`;
+  } else if (totalSocialRate > standardRate) {
+    return `The most profitable rate for exchange is <strong>standard rate</strong>.`;
+  } else {
+    return `Both rates are equal.`;
+  }
+}
 
 function clearInputs() {
   document.getElementById("fee").value = null;
   document.getElementById("social-rate").value = null;
   document.getElementById("standard-rate").value = null;
-  document.getElementById("fee-result").innerHTML = "";
+  document.getElementById("result-container").innerHTML = "";
 }
